923f20bbbfbfcee4b86f3b119e6433177073fbdb
tgispert <spfury@gmail.com>
2017-05-09 19:52:29 -0300

    seleccion de marca + pipe no probado

--- src/app/pipes/pricing-search-name.pipe.ts
+++ src/app/pipes/pricing-search-name.pipe.ts
@@ -8,7 +8,7 @@ export class PricingSearchNamePipe implements PipeTransform {
 
   transform(items: any, arg1: string): any {
     let filter = arg1.toLocaleLowerCase();
-    return filter ? items.filter(item => ((item.name.toLocaleLowerCase().indexOf(filter) !== -1) || (item.code.toLocaleLowerCase().indexOf(filter) !== -1))) : items;
+    return filter ? items.filter(item => ((item.name.toLocaleLowerCase().indexOf(filter) !== -1) || (item.code.toLocaleLowerCase().indexOf(filter) !== -1) || (item.class.toLocaleLowerCase().indexOf(filter) !== -1))) : items;
   }
 
 }
